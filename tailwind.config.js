ebac3a168dfd5d307e1b42402a79762f34bde296
coder-dom <giangtruong100702@gmail.com>
2024-09-21 12:54:59 +0700

    khởi tạo dự án

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    
+    /** Customizing Screens */
+    theme: {
+        'sm': '575.98px',
+        // => @media (min-width: 575.98px) { ... }
+
+        'md': '767.98px',
+        // => @media (min-width: 767.98) { ... }
+
+        'lg': '991.98px',
+        // => @media (min-width: 991.98) { ... }
+
+        'xl': '1239.98px',
+        // => @media (min-width: 1239.98) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+    },
+
+    /** Adding additional colors */ 
+    extend: {
+        colors: {
+            primary: '#000000',
+            secondary: '#00000099'
+        },
+    }
+  }
