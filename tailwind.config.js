93e0cc13ad24b566a0ef3294b6113a821cf99f5c
coder-dom <giangtruong100702@gmail.com>
2024-09-21 13:16:19 +0700

    top header : xong

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 tailwind.config = {
-    
-    /** Customizing Screens */
     theme: {
-        'sm': '575.98px',
-        // => @media (min-width: 575.98px) { ... }
+        /** Customizing Screens */
+        screens: {
+            'sm': '575.98px',
+            // => @media (min-width: 575.98px) { ... }
 
-        'md': '767.98px',
-        // => @media (min-width: 767.98) { ... }
+            'md': '767.98px',
+            // => @media (min-width: 767.98) { ... }
 
-        'lg': '991.98px',
-        // => @media (min-width: 991.98) { ... }
+            'lg': '991.98px',
+            // => @media (min-width: 991.98) { ... }
 
-        'xl': '1239.98px',
-        // => @media (min-width: 1239.98) { ... }
+            'xl': '1239.98px',
+            // => @media (min-width: 1239.98) { ... }
 
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-    },
-
-    /** Adding additional colors */ 
-    extend: {
-        colors: {
-            primary: '#000000',
-            secondary: '#00000099'
+            '2xl': '1239.98px',
+            // => @media (min-width: 1536px) { ... }
         },
-    }
+        
+        /** Adding additional colors */ 
+        extend: {
+            colors: {
+                primary: '#000000',
+                secondary: '#00000099'
+            },
+        }
+    },
   }
